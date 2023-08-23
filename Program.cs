2ab918f29ff939a909c7ec56fe031d1128a8da4e
Alumno <Alumno@L29-25433020>
2023-08-23 11:48:30 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using ExpendedorG2_2024_1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        expendedora miexpendedora = new expendedora();
+    }
+}
---
+++ expendedora.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading; //necesaria para hacer la pausa de 2 seg
+
+namespace ExpendedorG2_2024_1
+{
+    internal class expendedora
+    {
+        #region atributos
+        //los atributos siempre la maquina le asigna un valor por dfault
+        private string marca;
+        private ushort cantproductos;
+        private float precio;
+
+        #endregion
+        #region metodos
+        private void limpiar()
+        {
+            Thread.Sleep(2000); // hace una pausa en la terminal de 2 segundos
+            Console.Clear();
+
+        }
+        private void saludar()
+        {
+            Console.WriteLine("Bienvenido, elige un producto");
+
+        }
+        private string MostrarProducto()
+
+        {
+            Console.WriteLine("3A: Doritos\n3B: Churrumais");
+            Console.WriteLine("Ingresa el codigo del producto");
+            string codigo= Console.ReadLine(); // aqui el "codigo" esta dentro de un metodo, por lo que es una variable local, si no tuiera el mismo nombre que un atributo abria que inicializarlo dentro del metodo
+
+            return codigo;
+        }
+        private void MostrarPrecio(string codigo)
+        {
+            switch (codigo)
+            {
+                case "3A":
+                    Console.WriteLine("precio:{0}", precio);
+                    break;
+                case "3B":
+                    Console.WriteLine("precio:{0}", precio-6);
+                    break;
+                default:
+                    Console.WriteLine("no ingresaste un codigo valido");
+                    break;
+            }
+
+        }
+        #endregion
+        #region constructor
+        public expendedora()
+        {
+            precio = 18;
+            saludar();
+            limpiar();
+            marca = "AMS";
+            Console.WriteLine("MARCA: {0}",marca);
+            limpiar();
+            string codigo=MostrarProducto();
+            limpiar();
+            MostrarPrecio(codigo);
+        }
+        #endregion
+    }
+
+}
